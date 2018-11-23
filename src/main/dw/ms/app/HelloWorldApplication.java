e48c37a05c75f59cd3e56d3f39eb0216761df3cf
Rashad Javadov <rashjz@users.noreply.github.com>
2018-11-23 16:14:10 +0100

    Create HelloWorldApplication.java

---
+++ src/main/dw/ms/app/HelloWorldApplication.java
@@ -0,0 +1,91 @@
+package dw.ms.app;
+
+import dw.ms.app.core.Person;
+import dw.ms.app.db.PersonDAO;
+import dw.ms.app.resources.PersonResource;
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
+
+import java.util.Map;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    private final HibernateBundle<HelloWorldConfiguration> hibernateBundle =
+            new HibernateBundle<HelloWorldConfiguration>(Person.class) {
+                @Override
+                public DataSourceFactory getDataSourceFactory(HelloWorldConfiguration configuration) {
+                    return configuration.getDataSourceFactory();
+                }
+            };
+
+    @Override
+    public String getName() {
+        return "application";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(
+                        bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+
+//        bootstrap.addCommand(new RenderCommand());
+        bootstrap.addBundle(new AssetsBundle());
+        bootstrap.addBundle(new MigrationsBundle<HelloWorldConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(HelloWorldConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        });
+        bootstrap.addBundle(hibernateBundle);
+        bootstrap.addBundle(new ViewBundle<HelloWorldConfiguration>() {
+            @Override
+            public Map<String, Map<String, String>> getViewConfiguration(HelloWorldConfiguration configuration) {
+                return configuration.getViewRendererConfiguration();
+            }
+        });
+    }
+
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        final PersonDAO dao = new PersonDAO(hibernateBundle.getSessionFactory());
+//        final Template template = configuration.buildTemplate();
+
+//        environment.healthChecks().register("template", new TemplateHealthCheck(template));
+//        environment.admin().addTask(new EchoTask());
+//        environment.jersey().register(DateRequiredFeature.class);
+//        environment.jersey().register(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<User>()
+//                .setAuthenticator(new ExampleAuthenticator())
+//                .setAuthorizer(new ExampleAuthorizer())
+//                .setRealm("SUPER SECRET STUFF")
+//                .buildAuthFilter()));
+
+//        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
+//        environment.jersey().register(RolesAllowedDynamicFeature.class);
+
+
+//        environment.jersey().register(new HelloWorldResource(template));
+//        environment.jersey().register(new ViewResource());
+//        environment.jersey().register(new ProtectedResource());
+//        environment.jersey().register(new PeopleResource(dao));
+        environment.jersey().register(new PersonResource(dao));
+//        environment.jersey().register(new FilteredResource());
+    }
+}
